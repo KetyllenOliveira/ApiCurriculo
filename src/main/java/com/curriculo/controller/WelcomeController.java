c6ca0d75c827fab06bb3983b08294b94b0b33a4e
Ketyllen Emilly <emyllykety@gmail.com>
2024-09-29 18:36:21 -0300

    refac error

--- src/main/java/com/curriculo/controller/WelcomeController.java
+++ src/main/java/com/curriculo/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.curriculo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")  // Mapeia a raiz da aplicação
 public class WelcomeController {
 
-    @GetMapping
-    public String welcomeMessage() {
-        return "Hola, seja bem-vinda à central de currículos!";
-    }
+@GetMapping("/")
+public ResponseEntity<String> welcomeMessage() {
+    return ResponseEntity.ok("Hola, seja bem-vinda à central de currículos!");
+}
+
 }
 
 
