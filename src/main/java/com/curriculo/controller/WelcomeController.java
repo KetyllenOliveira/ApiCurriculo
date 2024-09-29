45b1839cafca7687bd47c1e5238faeeaff8ff7fe
Ketyllen Emilly <emyllykety@gmail.com>
2024-09-29 18:41:23 -0300

    refac

--- src/main/java/com/curriculo/controller/WelcomeController.java
+++ src/main/java/com/curriculo/controller/WelcomeController.java
@@ -2,18 +2,15 @@ package com.curriculo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")  // Mapeia a raiz da aplicação
 public class WelcomeController {
 
-@GetMapping("/")
-public ResponseEntity<String> welcomeMessage() {
-    return ResponseEntity.ok("Hola, seja bem-vinda à central de currículos!");
-}
-
+    @GetMapping("/")
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Bem-vindo à API de Currículos!");
+    }
 }
 
 
