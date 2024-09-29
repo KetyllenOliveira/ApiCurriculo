e3a5312657ecd03d7c9d5a407f8d0c109964686f
Ketyllen Emilly <emyllykety@gmail.com>
2024-09-29 19:14:05 -0300

    refac

--- src/main/java/com/curriculo/controller/WelcomeController.java
+++ src/main/java/com/curriculo/controller/WelcomeController.java
@@ -1,12 +1,13 @@
 package com.curriculo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*") // Adjust this for your security needs
 public class WelcomeController {
-
     @GetMapping("/")
     public ResponseEntity<String> welcome() {
         return ResponseEntity.ok("Bem-vindo à API de Currículos!");
