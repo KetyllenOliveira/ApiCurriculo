2e1116e6bed7ac8ab88c7437dee27601b1917c72
Ketyllen Oliveira <96392358+KetyllenOliveira@users.noreply.github.com>
2024-10-03 02:41:56 +0000

    refac componetscan

--- src/main/java/com/curriculo/controller/WelcomeController.java
+++ src/main/java/com/curriculo/controller/WelcomeController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/welcome")
-@ComponentScan(basePackages = "com.curriculo")
-
 
 public class WelcomeController {
 
